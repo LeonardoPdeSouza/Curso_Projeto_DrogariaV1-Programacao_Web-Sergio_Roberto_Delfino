00ecb530421c8bea7be3d2b568792926831513c0
leona <leona@Leonardo>
2016-10-17 10:38:14 -0200

    Adicionando mensagens de erro e sucesso; Adicionando botões editar e excluir; Corrigindo erros de filtragem

--- Curso_Projeto_DrogariaV1-Programacao_Web-Sergio_Roberto_Delfino/src/br/com/drogaria/bean/FabricanteBean.java
+++ Curso_Projeto_DrogariaV1-Programacao_Web-Sergio_Roberto_Delfino/src/br/com/drogaria/bean/FabricanteBean.java
@@ -6,16 +6,17 @@ import java.util.ArrayList;
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
-import javax.faces.model.ListDataModel;
 
 import br.com.drogaria.dao.FabricanteDAO;
 import br.com.drogaria.domain.Fabricante;
+import br.com.drogaria.util.JSFUtil;
 
 @ManagedBean(name = "MBFabricante")
 @ViewScoped
 public class FabricanteBean {
 	private Fabricante fabricante;
-	private ListDataModel<Fabricante> itens;
+	private ArrayList<Fabricante> itens;
+	private ArrayList<Fabricante> itensFiltrados;
 
 	public Fabricante getFabricante() {
 		return fabricante;
@@ -25,12 +26,20 @@ public class FabricanteBean {
 		this.fabricante = fabricante;
 	}
 
-	public ListDataModel<Fabricante> getItens() {
+	public ArrayList<Fabricante> getItens() {
 		return itens;
 	}
 
-	public void setItens(ListDataModel<Fabricante> itens) {
+	public void setItens(ArrayList<Fabricante> itens) {
 		this.itens = itens;
+	}
+
+	public ArrayList<Fabricante> getItensFiltrados() {
+		return itensFiltrados;
+	}
+
+	public void setItensFiltrados(ArrayList<Fabricante> itensFiltrados) {
+		this.itensFiltrados = itensFiltrados;
 	}
 	
 	public void prepararNovo() {
@@ -42,15 +51,12 @@ public class FabricanteBean {
 		try {
 
 			FabricanteDAO dao = new FabricanteDAO();
-
-			ArrayList<Fabricante> lista = dao.listar();
-
-			itens = new ListDataModel<Fabricante>(lista);
+			itens = dao.listar();
 
 		} catch (SQLException ex) {
 			ex.printStackTrace();
-			//JSFUtil.adicionarMensagemErro("Não foi possivel listar os fabricantes"
-			//		+ ex.getMessage() + "\n" + ex.getErrorCode());
+			JSFUtil.adicionarMensagemErro("Não foi possivel listar os fabricantes"
+					+ ex.getMessage() + "\n" + ex.getErrorCode());
 		}
 	}
 	
@@ -59,16 +65,48 @@ public class FabricanteBean {
 		try {
 			FabricanteDAO dao = new FabricanteDAO();
 			dao.salvar(fabricante);
+			itens = dao.listar();
 
-			ArrayList<Fabricante> lista = dao.listar();
-			itens = new ListDataModel<Fabricante>(lista);
-
-			//JSFUtil.adicionarMensagemSucesso("Fabricante cadastrado com sucesso !");
+			JSFUtil.adicionarMensagemSucesso("Fabricante cadastrado com sucesso !");
 
 		} catch (SQLException ex) {
 			ex.printStackTrace();
-			//JSFUtil.adicionarMensagemErro("Não foi Gravar o fabricante"
-			//		+ ex.getMessage() + "\n" + ex.getErrorCode());
+			JSFUtil.adicionarMensagemErro("Não foi Gravar o fabricante"
+					+ ex.getMessage() + "\n" + ex.getErrorCode());
 		}
 }
+
+	public void excluir() {
+
+		try {
+			FabricanteDAO dao = new FabricanteDAO();
+			dao.excluir(fabricante);
+			itens = dao.listar();
+
+			JSFUtil.adicionarMensagemSucesso("Fabricante Excluido com sucesso !");
+
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+			JSFUtil.adicionarMensagemErro("Não foi excluir o fabricante"
+					+ ex.getMessage() + "\n" + ex.getErrorCode());
+		}
+
+	}
+
+	public void editar() {
+
+		try {
+			FabricanteDAO dao = new FabricanteDAO();
+			dao.editar(fabricante);
+			itens = dao.listar();
+
+			JSFUtil.adicionarMensagemSucesso("Fabricante Editado com sucesso !");
+
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+			JSFUtil.adicionarMensagemErro("Não foi editar o fabricante"
+					+ ex.getMessage() + "\n" + ex.getErrorCode());
+		}
+
+	}
 }	
