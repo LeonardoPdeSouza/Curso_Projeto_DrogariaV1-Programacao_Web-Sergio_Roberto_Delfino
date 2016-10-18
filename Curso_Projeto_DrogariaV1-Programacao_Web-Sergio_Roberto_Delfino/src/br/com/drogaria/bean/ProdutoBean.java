0bf1dca37de8e6a4569d6293980ffb1c272418b7
leona <leona@Leonardo>
2016-10-17 22:34:20 -0200

    Concluido a parte de cadastro de produto

--- Curso_Projeto_DrogariaV1-Programacao_Web-Sergio_Roberto_Delfino/src/br/com/drogaria/bean/ProdutoBean.java
+++ Curso_Projeto_DrogariaV1-Programacao_Web-Sergio_Roberto_Delfino/src/br/com/drogaria/bean/ProdutoBean.java
@@ -6,9 +6,10 @@ import java.util.ArrayList;
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
-import javax.faces.model.ListDataModel;
 
+import br.com.drogaria.dao.FabricanteDAO;
 import br.com.drogaria.dao.ProdutoDAO;
+import br.com.drogaria.domain.Fabricante;
 import br.com.drogaria.domain.Produto;
 import br.com.drogaria.util.JSFUtil;
 
@@ -18,8 +19,8 @@ public class ProdutoBean {
 
 	private ArrayList<Produto> itens;
 	private ArrayList<Produto> itensFiltrados;
-	
-	
+	private Produto produto;
+	private ArrayList<Fabricante> comboFabricantes;
 
 	public ArrayList<Produto> getItens() {
 		return itens;
@@ -36,8 +37,22 @@ public class ProdutoBean {
 	public void setItensFiltrados(ArrayList<Produto> itensFiltrados) {
 		this.itensFiltrados = itensFiltrados;
 	}
+	
+	public Produto getProduto() {
+		return produto;
+	}
 
+	public void setProduto(Produto produto) {
+		this.produto = produto;
+	}
 
+	public ArrayList<Fabricante> getComboFabricantes() {
+		return comboFabricantes;
+	}
+
+	public void setComboFabricantes(ArrayList<Fabricante> comboFabricantes) {
+		this.comboFabricantes = comboFabricantes;
+	}
 
 	@PostConstruct
 	public void CarregarListagem() {
@@ -49,6 +64,32 @@ public class ProdutoBean {
 		} catch (SQLException ex) {
 			ex.printStackTrace();
 			JSFUtil.adicionarMensagemErro(ex.getMessage());
+		}
+	}
+	
+	public void prepararNovo() {
+		try{
+		produto = new Produto();
+		
+		FabricanteDAO dao = new FabricanteDAO();
+		
+		comboFabricantes = dao.listar();
+		} catch (Exception e) {
+			e.printStackTrace();
+			JSFUtil.adicionarMensagemErro(e.getMessage());
+		}
+		
+		
+	}
+	
+	public void novo(){
+		try{
+		ProdutoDAO dao = new ProdutoDAO();
+		dao.salvar(produto);
+		itens = dao.listar();
+		} catch (Exception e) {
+			e.printStackTrace();
+			JSFUtil.adicionarMensagemErro(e.getMessage());
 		}
 	}
 
